f1e6a381ae54ae7f8df66325a68e37e899601465
Bao Tran <brightsmile959674@gmail.com>
2016-04-25 22:05:46 +0700

    Load data form json files

--- LinkedIn/app/app.js
+++ LinkedIn/app/app.js
@@ -1,12 +1,36 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+var app = angular.module('myApp', []);
+
+app.controller('myCtrl', function ($scope, $http) {
+  // Load summary
+  $http.get('./json/summary.json').success(function(data) {
+    $scope.summary = data;
+  });
+
+  // Load projects
+  $http.get('./json/project.json').success(function(data) {
+    $scope.project = data;
+  });
+
+  // Load education
+  $http.get('./json/education.json').success(function(data) {
+    $scope.education = data;
+  });
+
+  // Load skills
+  $http.get('./json/skill.json').success(function(data) {
+    $scope.skill = data;
+  });
+
+  // Load experience
+  $http.get('./json/experience.json').success(function(data) {
+    $scope.experience = data;
+  });
+
+  // Load basic info
+  $http.get('./json/basicinfo.json').success(function(data) {
+    $scope.basicinfo = data;
+  });
+});
